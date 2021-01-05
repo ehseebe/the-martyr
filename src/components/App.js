568ab19dc8864c491b491bca73f409e5ea4ec03c
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2021-01-05 12:57:44 -0500

    implemented autoplay video

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Martyr from './video/martyr-video.mp4';
+import './App.css';
 
 const App = () => {
   return (
     <div>
       Hello!
+      <video className="martyr-video" autoPlay loop muted>
+        <source src={Martyr} type='video/mp4' />
+      </video>
     </div>
   )
 }
