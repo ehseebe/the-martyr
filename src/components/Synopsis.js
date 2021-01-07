b299eefac94bc7e8e4e9779bab47e2126c9d6e44
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2021-01-07 14:56:49 -0500

    synopsis content, prelim style

--- src/components/Synopsis.js
+++ src/components/Synopsis.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import './Synopsis.css';
 
 const Synopsis = () => {
   return (
-    <h1> Synopsis </h1>
-  )
-}
+    <section>
+      <div className="synopsis">
+        <h1 className="title"> The Martyr </h1>
+        <p className="subhead">
+          They say never meet your heroes. But what if your hero, like Margot K.
+          Juby’s, is a 15th century serial killer? Gilles de Rais was a
+          nobleman, decorated military statesman—and one of the grisliest
+          murderers in recorded history. Or, at least, that’s what history tells
+          us. But Margot’s fanatical, 20-year long research project suggests
+          otherwise. According to her, de Rais was the victim of a vast
+          conspiracy. She’s so sure that she has “Gilles de Rais was innocent”
+          tattooed on her arm. <br/><br/>
+          
+          The Martyr interweaves the deeply personal story
+          of how Margot came to fall in love with de Rais, with a gothic,
+          animated retelling of the nobleman’s life. Along the way, the film
+          uncovers an amusing plot by the French intelligentsia to exonerate the
+          nobleman, and a potential conspiracy at the heart of the medieval
+          French court. Painting an eerie, paranoid—yet compassionate—portrait
+          of unrequited love, The Martyr is a film about the dark obsessions
+          that shape all of us.
+        </p>
+      </div>
+    </section>
+  );
+};
 
 export default Synopsis;
