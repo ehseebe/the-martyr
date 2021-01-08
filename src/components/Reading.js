171b922fbf6fa35940498e2ea775a8c98bba342c
Alyssa Beaton Tanguay <ehseebe@gmail.com>
2021-01-08 09:33:03 -0500

    card styling and animation

--- src/components/Reading.js
+++ src/components/Reading.js
@@ -4,7 +4,7 @@ import "./Reading.css";
 const Reading = () => {
   return (
     <section>
-      <article className="further-reading">
+      <article className="further-reading fade-in">
         {/* <h1 className="title">Reading</h1> */}
         <p>
           Gilles de Rais has captured the European artistic imagination for
@@ -15,7 +15,9 @@ const Reading = () => {
           what you think.{" "}
         </p>
 
-        <div className="reading-category">
+        <div className="reading-category-card-grid">
+
+        <div className="reading-category-card">
           <h2>Literary Fiction</h2>
           <ul>
             <li>1891: Là-bas, JK Huysmans</li>
@@ -26,7 +28,7 @@ const Reading = () => {
           </ul>
         </div>
 
-        <div className="reading-category">
+        <div className="reading-category-card">
           <h2>Comic Books</h2>
           <ul>
             <li>1972: Le Crapouillot - N° 22 : Love & Magic, from Gilles de Rais to Rasputin, various</li>
@@ -34,7 +36,7 @@ const Reading = () => {
           </ul>
         </div>
 
-        <div className="reading-category">
+        <div className="reading-category-card">
           <h2>Non-fiction/True Crime</h2>
           <ul>
             <li>1858: Curiosités de l’histoire de la France, Paul Lacroix</li>
@@ -48,6 +50,30 @@ const Reading = () => {
             <li>2012: Gilles de Rais, Matei Cazacu</li>
             <li>2016: The Horrific Crimes of Gilles de Rais Revisited, Jack Smith</li>
           </ul>
+        </div>
+
+        <div className="reading-category-card">
+          <h2>Academic Writing</h2>
+
+          <ul>
+            <li>1905: Gilles de Rais, Salomon Reinach</li>
+            <li>1992: Gilles de Rais, de Gilbert Prouteau, ou le « Naufrage de L'Histoire », Jean Kerhervé</li>
+            <li>2012: Sympathy for the Devil : Gilles de Rais and His Modern Apologists, Ben Parsons</li>
+            <li>2016: Medieval Religion and its Anxieties : History and Mystery in the Other Middle Ages, Thomas A. Fudgé</li>
+          </ul>
+        </div>
+
+        <div className="reading-category-card">
+          <h2>Other Media</h2>
+
+          <ul>
+            <li>1893: Gilles de Retz (opera), Paul Ladmirault</li>
+            <li>1962: Films sans images: Gilles de Rais (radio play) Blaise Cendrars</li>
+            <li>1993: Le Tombeau de Gilles de Rais (opera), Edith Canat de Chizy</li>
+            <li>2008: Godspeed on the Devil’s Thunder (music) Cradle of Filth</li>
+            <li>2014: Monstrum : le terrifiant destin de Gilles de Rais (film)</li>
+          </ul>
+        </div>
         </div>
       </article>
     </section>
