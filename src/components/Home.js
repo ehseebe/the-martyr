35017691377f962f23a6f1432b4573c55926935a
Alyssa BT <ehseebe@gmail.com>
2021-01-17 15:58:48 -0500

    cta button styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,30 @@
-import React from 'react';
-import Martyr from './video/martyr-video.mp4';
-import './App.css';
-import './Home.css';
+import React from "react";
+import Martyr from "./video/martyr-video.mp4";
+import "./App.css";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <section>
+    <>
+    <video className="martyr-video" autoPlay loop muted>
+          <source src={Martyr} type="video/mp4" />
+        </video>
 
-<div className="main-title-overlay">
-      <h1 className="main-title fade-in-bottom">The Martyr</h1>
-      <h4 className="main-subtitle fade-in-top">A film by Edmund Stenson</h4>
-      </div>
+      <section>
+        <div className="main-title-overlay">
+          <h1 className="main-title fade-in-bottom">The Martyr</h1>
+          <h4 className="main-subtitle fade-in-top">
+            A short film by Edmund Stenson
+          </h4>
 
-      <video className="martyr-video" autoPlay loop muted>
-        <source src={Martyr} type='video/mp4' />
+          <button className="homepage-cta"> enter </button>
+        </div>
+
         
-      </video>
-
-      <div className="homepage-synopsis">
-        <div className="synopsis">
-          <div className="synopsis-heading">
-            heading
-            <div className="synopsis-body">
-              
-            </div>
-          </div>
-        </div>
-        </div>
-
+      </section>
       
-
-      
-      
-    </section>
-  )
-}
+    </>
+  );
+};
 
 export default Home;
