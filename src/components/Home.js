e442b0daec7472f61906901f33005a5d9cc2bdc7
home <alyssa@macbook.local>
2021-01-17 15:03:50 -0500

    uncommented footer, added new home element

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import Martyr from './video/martyr-video.mp4';
 import './App.css';
+import './Home.css';
 
 const Home = () => {
   return (
     <section>
-      <video className="martyr-video" autoPlay loop muted>
-        <source src={Martyr} type='video/mp4' />
-      </video>
-      <div className="main-title-overlay">
+
+<div className="main-title-overlay">
       <h1 className="main-title fade-in-bottom">The Martyr</h1>
       <h4 className="main-subtitle fade-in-top">A film by Edmund Stenson</h4>
       </div>
+
+      <video className="martyr-video" autoPlay loop muted>
+        <source src={Martyr} type='video/mp4' />
+        
+      </video>
+
+      <div className="homepage-synopsis">
+        <div className="synopsis">
+          <div className="synopsis-heading">
+            heading
+            <div className="synopsis-body">
+              synopsis body
+            </div>
+          </div>
+        </div>
+        </div>
+
+      
+
+      
+      
     </section>
   )
 }
