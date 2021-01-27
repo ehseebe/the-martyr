5c29cd0012a131cc55d69009acb420c98d53874f
Alyssa BT <ehseebe@gmail.com>
2021-01-27 18:46:08 -0500

    Scaffolding for component

--- src/components/Innocent.js
+++ src/components/Innocent.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './Innocent.css';
+
 
 const Innocent = () => {
   return (
-    <h1> Innocent </h1>
+    <section className="container-body-innocent">
+
+      <div className="innocent__header">
+      <i><h1> Was Gilles de Rais innocent? </h1></i>
+      <div className="innocent__header--intro">
+      <p>Here we offer a number of interview excerpts in which Margot offers potential reasons for Gilles de Rais’ innocence. </p>
+      <p className="innocent__header--intro-crossout">These largely relate to the posthumous trial record.</p> 
+      <p>We leave it to you to make up your minds...</p>
+      </div>
+      </div>
+
+      <div className="innocent__content">
+        Videos
+      </div>
+    
+    </section>
   )
 }
 
