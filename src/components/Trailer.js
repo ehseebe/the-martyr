89731da4f3ed9510c4e7ad84127c66054d598b93
Alyssa BT <ehseebe@gmail.com>
2021-08-11 22:15:24 -0400

    Adjust Trailer

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -1,20 +1,17 @@
 import React from 'react';
-// import ReactPlayer from 'react-player/vimeo';
+import ReactPlayer from 'react-player/vimeo';
 import './Trailer.css';
 
 const Trailer = () => {
   return (
     <section className="trailer">
-      {/* <ReactPlayer
+      <ReactPlayer
       className="react-player" 
       url="https://vimeo.com/503848711" 
       playing="true"
-      // width='100%'
-      // height='80%'
       loop="true"
-      /> */}
-   
-      <iframe src="https://player.vimeo.com/video/503848711?h=2725ebb85d" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="The Martyr Film"></iframe>
+      title="The Martyr Film"
+      />
     </section>
   )
 }
